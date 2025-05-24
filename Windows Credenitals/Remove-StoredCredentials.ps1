f8b4e1bc4f3d4665d4faf337113f4042a76c373c
Shehab1Noaman <78321126+Shehab1Noaman@users.noreply.github.com>
2025-05-24 12:03:59 +0100

    Add files via upload

---
+++ Windows Credenitals/Remove-StoredCredentials.ps1
@@ -0,0 +1,19 @@
+﻿Try {
+    # Get a list of stored credentials
+    $creds = cmdkey /list | Where-Object { $_ -match "Target:" } | ForEach-Object {
+        ($_ -split ":")[1].Trim()
+    }
+
+    # Delete each credential
+    foreach ($target in $creds) {
+        Write-Host "Deleting credential: $target"
+        cmdkey /delete:$target
+    }
+
+    Write-Output "Script executed successfully"
+    Exit 0
+}
+Catch {
+    Write-Error "Script failed: $_"
+    Exit 1
+}
